df34c73bd3c19ef1c5f111d02071c84329beed32
Stephen D. Gibson <Stephen.d.gibson@gmail.com>
2020-04-05 17:21:12 -0400

    generates a table of countries as static content

--- create_country_menu.py
+++ create_country_menu.py
@@ -14,15 +14,16 @@ def create_country_menu():
     # get the list of keys from the map and sort them (just to be sure)
     letters = sorted(name_map.keys())
     # build up the menu for GitHub.io (navbar in bootstrap)
-    output = '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >All Countries</a>\n'
-    output += '\t<div class="dropdown-menu" aria-labelledby="navbarDropdown">\n'
+    output = '<div class="container">\n'
     for l in letters:
         list_countries = sorted(name_map[l])
         label = list_countries[0] + '...' + list_countries[len(list_countries) - 1]
-        output += '\t\t<a class="dropdown-item" href="#">{}</a>\n'.format(label)
+        output += '    <div class="row"><a class="dropdown-item" href="javascript:$(\'#{}\').toggleClass(\'collapse\')">{}</a></div>\n'.format(l, label)
+        output += '    <div class="collapse" id="{}">\n'.format(l)
         for c in list_countries:
-            output += '\t\t\t<a class="dropdown-item" href="javascript:country_handler(\'{}\')">{}</a>\n'.format(c.replace(' ', '-'), c)
-    output += '\t</div>'
+            output += '            <a class="dropdown-item" href="javascript:country_handler(\'{}\')">{}</a>\n'.format(c.replace(' ', '-'), c)
+        output += '    </div>\n'
+    output += '</div>\n'
     with (open('output.html', 'w')) as f:
         f.write(output)
 
